c7386cb2e0b961570fa40dff7e40e3c121556dec
slyfromhun <sfox5948@gmail.com>
2025-09-09 22:58:03 +0200

    Effect changes

--- data/vehicle/shared/script/vehicle_effects_shared.gd
+++ data/vehicle/shared/script/vehicle_effects_shared.gd
@@ -7,9 +7,10 @@ extends Node
 @export var grindScenes: Array[GPUParticles2D]
 @export var chassisScenes: Array[GPUParticles2D]
 
-var wheelCol: Node
+var wheelCol: Array[Node]
 var wheelRB: Node
 var chassis: Node
+var power_distribution: int
 
 var slip_ratio_rear: float
 var speed_kph: float
@@ -20,10 +21,10 @@ func _ready() -> void:
 	initalize()
 
 func _physics_process(_delta: float) -> void:
-	slip_ratio_rear = abs(chassis.slip_ratio_rear)
-	speed_kph = abs(chassis.kph)
-	angular_speed_kph = abs(chassis.wheel_angular_kph)
-	wheel_magnitude = chassis.wheel_magnitude_rear
+	slip_ratio_rear = chassis.slip_ratios[power_distribution]
+	speed_kph = chassis.kph
+	angular_speed_kph = abs(chassis.wheels_angular_kph[power_distribution])
+	wheel_magnitude = chassis.wheels_angular_magnitude[power_distribution]
 
 func _process(_delta: float) -> void:
 	effect.friction(angular_speed_kph, frictionScenes, wheel_magnitude, slip_ratio_rear)
@@ -32,16 +33,17 @@ func _process(_delta: float) -> void:
 	effect.dust(speed_kph, chassisScenes)
 
 func initalize():
-	wheelCol = get_tree().get_first_node_in_group("wheels_coll")
+	wheelCol = get_tree().get_nodes_in_group("wheels_coll")
 	wheelRB = get_tree().get_first_node_in_group("wheels")
 	chassis = get_tree().get_first_node_in_group("chassis")
+	power_distribution = chassis.transmission.power_distribution
 	###
 	for scene in frictionScenes:
-		scene.global_position = wheelCol.global_position
+		scene.global_position = wheelCol[power_distribution].global_position
 	for scene in slipScenes:
-		scene.global_position = wheelCol.global_position
+		scene.global_position = wheelCol[power_distribution].global_position
 	for scene in grindScenes:
-		scene.global_position = wheelCol.global_position + Vector2(0, chassis.tire.radius)
+		scene.global_position = wheelCol[power_distribution].global_position + Vector2(0, chassis.tire.radius)
 	for scene in chassisScenes:
 		scene.global_position = chassis.global_position
 	###
