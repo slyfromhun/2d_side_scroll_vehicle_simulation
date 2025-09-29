6750d6391ea816cf06e7fa5f71515d7869af54e6
slyfromhun <sfox5948@gmail.com>
2025-09-29 09:28:32 +0200

    Vehicle Effect changes
    - Effects now work for both wheels

--- data/resource/effects_vehicle_functions.gd
+++ data/resource/effects_vehicle_functions.gd
@@ -4,52 +4,66 @@ extends Resource
 @export_group("Friction")
 @export var _friction := true
 @export var friction_emit_power: Curve
-@export var speed_friction := [0, 666]
+@export var speed_friction := [70, 666]
 @export_group("Slip")
 @export var _slip := true
 @export var slip_emit_power: Curve
-@export var speed_slip := [0, 666]
+@export var speed_slip := [4, 666]
+@export var range_slip := 5
 @export_group("Grind")
 @export var _grind := true
 @export var grind_emit_power: Curve
-@export var speed_grind := [0, 666]
+@export var speed_grind := [4, 666]
+@export var range_grind := 5
 @export_group("Dust")
 @export var _dust := true
 @export var dust_color_power: Curve
-@export var speed_dust := [0, 666]
+@export var speed_dust := [90, 666]
 
-func friction(speed_kph: float, frictionScenes: Array[GPUParticles2D], wheel_magnitude: float, slip_ratio: float):
+func friction(speed_kph:float, frictionScenes:Array[GPUParticles2D], wheel_magnitude:float, slip_ratio:float):
 	if _friction:
-		if speed_friction[0] < speed_kph and speed_kph < speed_friction[1]:
+		if int(wheel_magnitude) == 0: wheel_magnitude = 1
+		if (speed_friction[0] < speed_kph and speed_kph < speed_friction[1]):
 			for scene in frictionScenes:
 				scene.process_material.direction.x = -wheel_magnitude
 				scene.amount_ratio = friction_emit_power.sample(abs(slip_ratio))
+		else:
+			for scene in frictionScenes:
+				scene.amount_ratio = 0
 	else:
 		for scene in frictionScenes:
 				scene.amount_ratio = 0
 
-func slip(speed_kph: float, slipScenes: Array[GPUParticles2D], wheel_magnitude: float, slip_ratio: float):
+func slip(speed_kph:float, speed_mps:float, slipScenes:Array[GPUParticles2D], wheel_magnitude:float, slip_ratio:float):
 	if _slip:
-		if (speed_slip[0] < speed_kph and speed_kph < speed_slip[1]):
+		if int(wheel_magnitude) == 0: wheel_magnitude = 1
+		if (speed_slip[0] < speed_kph and speed_kph < speed_slip[1]) or (speed_mps > range_slip):
 			for scene in slipScenes:
-				scene.process_material.direction.x = -wheel_magnitude
+				scene.process_material.direction.x = int(-wheel_magnitude)
 				scene.amount_ratio = slip_emit_power.sample(abs(slip_ratio))
+		else:
+			for scene in slipScenes:
+				scene.amount_ratio = 0
 	else:
 		for scene in slipScenes:
 				scene.amount_ratio = 0
 
-func grind(speed_kph: float, grindScenes: Array[GPUParticles2D], wheel_magnitude: float, slip_ratio: float):
+func grind(speed_kph:float, speed_mps:float, grindScenes:Array[GPUParticles2D], wheel_magnitude:float, slip_ratio:float):
 	if _grind:
-		if speed_grind[0] < speed_kph and speed_kph < speed_grind[1]:
+		if int(wheel_magnitude) == 0: wheel_magnitude = 1
+		if (speed_grind[0] < speed_kph and speed_kph < speed_grind[1]) or (speed_mps > range_grind):
 			grind_emit_power.set_point_offset(0, clampf(lerpf(0.04, 0.0, speed_kph / 20.0), 0.0, 0.04))
 			for scene in grindScenes:
-				scene.process_material.direction.x = -wheel_magnitude
+				scene.process_material.direction.x = int(-wheel_magnitude)
 				scene.amount_ratio = grind_emit_power.sample(abs(slip_ratio))
+		else:
+			for scene in grindScenes:
+				scene.amount_ratio = 0
 	else:
 		for scene in grindScenes:
 				scene.amount_ratio = 0
 
-func dust(speed_kph: float, chassisScenes: Array[GPUParticles2D]):
+func dust(speed_kph:float, chassisScenes:Array[GPUParticles2D]):
 	if _dust:
 		dust_color_power.set_point_offset(0, speed_dust[0])
 		dust_color_power.set_point_offset(1, speed_dust[1])
@@ -57,6 +71,9 @@ func dust(speed_kph: float, chassisScenes: Array[GPUParticles2D]):
 			for scene in chassisScenes:
 				scene.amount_ratio = 1
 				scene.modulate = Color(255, 255, 255, dust_color_power.sample(speed_kph))
+		else:
+			for scene in chassisScenes:
+				scene.amount_ratio = 0
 	else:
 		for scene in chassisScenes:
 				scene.amount_ratio = 0
--- data/vehicle/shared/script/vehicle_effects_shared.gd
+++ data/vehicle/shared/script/vehicle_effects_shared.gd
@@ -1,50 +1,53 @@
 class_name Vehicle_Effects
 extends Node
 
-@export var effect : VehicleEffects
-@export var frictionScenes: Array[GPUParticles2D]
-@export var slipScenes: Array[GPUParticles2D]
-@export var grindScenes: Array[GPUParticles2D]
+@export var effect: VehicleEffects
+@export var frictionScenes_wheel1: Array[GPUParticles2D]
+@export var frictionScenes_wheel2: Array[GPUParticles2D]
+@export var slipScenes_wheel1: Array[GPUParticles2D]
+@export var slipScenes_wheel2: Array[GPUParticles2D]
+@export var grindScenes_wheel1: Array[GPUParticles2D]
+@export var grindScenes_wheel2: Array[GPUParticles2D]
 @export var chassisScenes: Array[GPUParticles2D]
 
-var wheelCol: Array[Node]
-var wheelRB: Node
-var chassis: Node
-var power_distribution: int
+var frictionScenes: Array
+var slipScenes: Array
+var grindScenes: Array
 
-var slip_ratio_rear: float
-var speed_kph: float
-var wheel_magnitude: float
-var angular_speed_kph: float
+var wheelCol: Array[Node]
+var wheelRB: Array[Node]
+var chassis: Node
 
 func _ready() -> void:
 	initalize()
 
 func _physics_process(_delta: float) -> void:
-	slip_ratio_rear = chassis.slip_ratios[power_distribution]
-	speed_kph = chassis.kph
-	angular_speed_kph = abs(chassis.wheels_angular_kph[power_distribution])
-	wheel_magnitude = chassis.wheels_angular_magnitude[power_distribution]
+	pass
 
 func _process(_delta: float) -> void:
-	effect.friction(angular_speed_kph, frictionScenes, wheel_magnitude, slip_ratio_rear)
-	effect.slip(angular_speed_kph, slipScenes, wheel_magnitude, slip_ratio_rear)
-	effect.grind(angular_speed_kph, grindScenes, wheel_magnitude, slip_ratio_rear)
-	effect.dust(speed_kph, chassisScenes)
+	for i in wheelRB.size():
+		effect.friction(abs(chassis.wheels_angular_kph[i]), frictionScenes[i], chassis.wheels_angular_magnitude[i], chassis.slip_ratios[i])
+		effect.slip(abs(chassis.wheels_angular_kph[i]), chassis.wheels_mps[i], slipScenes[i], chassis.wheels_angular_magnitude[i], chassis.slip_ratios[i])
+		effect.grind(abs(chassis.wheels_angular_kph[i]), chassis.wheels_mps[i], grindScenes[i], chassis.wheels_angular_magnitude[i], chassis.slip_ratios[i])
+		effect.dust(chassis.kph, chassisScenes)
 
 func initalize():
 	wheelCol = get_tree().get_nodes_in_group("wheels_coll")
-	wheelRB = get_tree().get_first_node_in_group("wheels")
+	wheelRB = get_tree().get_nodes_in_group("wheels")
 	chassis = get_tree().get_first_node_in_group("chassis")
-	power_distribution = chassis.transmission.power_distribution
+
+	frictionScenes = [frictionScenes_wheel1, frictionScenes_wheel2]
+	slipScenes = [slipScenes_wheel1, slipScenes_wheel2]
+	grindScenes = [grindScenes_wheel1, grindScenes_wheel2]
 	###
-	for scene in frictionScenes:
-		scene.global_position = wheelCol[power_distribution].global_position
-	for scene in slipScenes:
-		scene.global_position = wheelCol[power_distribution].global_position
-	for scene in grindScenes:
-		scene.global_position = wheelCol[power_distribution].global_position + Vector2(0, chassis.tire.radius)
-	for scene in chassisScenes:
-		scene.global_position = chassis.global_position
+	for i in wheelRB.size():
+		for frictionScene in frictionScenes[i]:
+			frictionScene.global_position = wheelCol[i].global_position
+		for slipScene in slipScenes[i]:
+			slipScene.global_position = wheelCol[i].global_position
+		for grindScene in grindScenes[i]:
+			grindScene.global_position = wheelCol[i].global_position + Vector2(0, chassis.tire.radius)
+		for scene in chassisScenes:
+			scene.global_position = chassis.global_position
 	###
 	effect.dust_color_power.set_point_offset(0, effect.speed_dust[0])
