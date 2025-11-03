7bc4d55eadda999286da88a0f3c29f5642d97a01
slyfromhun <sfox5948@gmail.com>
2025-11-03 17:31:47 +0100

    Added Friction Static and Slide

--- data/resource/tire.gd
+++ data/resource/tire.gd
@@ -6,6 +6,10 @@ extends Resource
 ## Rolling Resistance coefficient, the bigger the number, the higher the resistance gets when the vehicle moves.
 @export_range(0.008, 0.039, 0.0001, "suffix:Crr") var rolling_resistance := 0.013
 ## Static and Slip longitudinal friction.
-@export var lon_friction := [1.46, 1.38]
+@export var lon_friction := [1.46, 1.06]
+## Friction Static Peak Grip
+@export_range(0.01, 100.0, 0.01, "suffix:%") var peak_friction_grip := 10.0
+## Friciton Slide Low Peak Grip
+@export_range(0.01, 100.0, 0.01, "suffix:%") var slide_friciton_grip := 100.0
 ## Friction multiplier curve depending on tire load.
 @export var lon_load_sensitivity := [-0.1, 10300]
