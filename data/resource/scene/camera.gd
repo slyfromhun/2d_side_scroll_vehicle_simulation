634ec76c8341c0f7d1871ad3515d8b248b0d7e91
slyfromhun <sfox5948@gmail.com>
2026-04-23 14:45:40 +0200

    Camera script refactor

--- data/resource/scene/camera.gd
+++ data/resource/scene/camera.gd
@@ -1,21 +1,41 @@
 class_name CameraStats
 extends Resource
 
+@export_group("Shake")
+@export var _shake := true
 @export var camera_shake: Curve
+@export_group("Zoom")
+@export var _zoom := true
 @export var camera_zoom: Curve
-@export var camera_follow := 36.0
-@export var offset_max := 4.0
 @export var zoom_max := 0.016
+@export_group("Offset")
+@export var _offset := true
+@export var offset_max := 4.0
+@export_group("Follow")
+@export var _follow := true
+@export var camera_follow := 36.0
 
 func camshake(offset:float, kph:float, top_power_speed:float, shake_curve:Curve) -> float:
-	return randf_range(-offset, offset) * shake_curve.sample(kph / top_power_speed)
+	if _shake:
+		return randf_range(-offset, offset) * shake_curve.sample(kph / top_power_speed)
+	else:
+		return 0.0
 
 func camzoom(zoom:float, wheel_rpm:float, red_line_rpm:float, zoom_curve:Curve) -> Vector2:
-	var camZoom = 1 + randf_range(-zoom, zoom) * zoom_curve.sample(wheel_rpm / (red_line_rpm))
-	return Vector2(camZoom, camZoom)
+	if _zoom:
+		var camZoom = 1 + randf_range(-zoom, zoom) * zoom_curve.sample(wheel_rpm / (red_line_rpm))
+		return Vector2(camZoom, camZoom)
+	else:
+		return Vector2.ZERO
 
 func camoffset(accel:float) -> float:
-	return 36.0 * -accel
+	if _offset:
+		return 36.0 * -accel
+	else:
+		return 0.0
 
 func camfollow(accel:float) -> float:
-	return clampf(36.0 - accel * 3.6, 4.0, 36.0)
+	if _follow:
+		return clampf(36.0 - accel * 3.6, 4.0, 36.0)
+	else:
+		return 0.0
--- data/vehicle/shared/script/vehicle_camera_shared.gd
+++ data/vehicle/shared/script/vehicle_camera_shared.gd
@@ -7,10 +7,8 @@ var kph: float
 var wheel_rpm: float
 var red_line_rpm: float
 var red_line_power: float
-var rpm_limit: float
 var top_power_speed: float
 var accel: float
-var camZoom: float
 
 var ChassisRB: RigidBody2D
 
@@ -18,22 +16,16 @@ var ChassisRB: RigidBody2D
 func _ready() -> void:
 	initalize()
 
-
 func _process(_delta: float) -> void:
 	kph = ChassisRB.kph
 	wheel_rpm = ChassisRB.wheel_rpm
 	accel = ChassisRB.acceleration
 	top_power_speed = ChassisRB.top_power_speed
 
-	var tween := create_tween()
-	#var tween2 := create_tween()
-	var tween3 := create_tween()
-	var tween4 := create_tween()
-
-	tween.tween_property(self, "offset", Vector2(0, camera.camshake(camera.offset_max, kph, top_power_speed, camera.camera_shake)), 0.041)
-	#tween2.tween_property(self, "offset", Vector2(camera.camoffset(accel), 0), 2.6)
-	tween3.tween_property(self, "zoom", camera.camzoom(camera.zoom_max, wheel_rpm, red_line_rpm, camera.camera_zoom), 0.041)
-	tween4.tween_property(self, "position_smoothing_speed", camera.camfollow(accel), 0.6)
+	camShake()
+	camOffset()
+	camZoom()
+	camFollow()
 
 func initalize():
 	ChassisRB = get_tree().get_first_node_in_group("chassis")
@@ -41,3 +33,20 @@ func initalize():
 	red_line_power = ChassisRB.engine.red_line_power
 
 	camera.camera_zoom.set_point_offset(0, red_line_power)
+
+
+func camShake():
+	var tween := create_tween()
+	tween.tween_property(self, "offset", Vector2(0, camera.camshake(camera.offset_max, kph, top_power_speed, camera.camera_shake)), 0.041)
+
+func camOffset():
+	var tween := create_tween()
+	tween.tween_property(self, "offset", Vector2(camera.camoffset(accel), 0), 2.6)
+
+func camZoom():
+	var tween := create_tween()
+	tween.tween_property(self, "zoom", camera.camzoom(camera.zoom_max, wheel_rpm, red_line_rpm, camera.camera_zoom), 0.041)
+
+func camFollow():
+	var tween := create_tween()
+	tween.tween_property(self, "position_smoothing_speed", camera.camfollow(accel), 0.6)
