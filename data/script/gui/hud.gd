19fccee5595f2e7db29941d4510b16314fbc65ff
slyfromhun <sfox5948@gmail.com>
2026-04-23 14:53:25 +0200

    Hud refactor
    - Engine curve for debug

--- data/script/gui/hud.gd
+++ data/script/gui/hud.gd
@@ -1,9 +1,11 @@
 extends CanvasLayer
 
+
+@export var calculate: HudFunctions
 @export_group("Tacho")
 @export var tacho: Control
 @export var rpm_progress: TextureProgressBar
-@export var rpm_redline_progress: TextureProgressBar
+@export var rpm_red_line_progress: TextureProgressBar
 @export var rpm_limit_progress: TextureProgressBar
 @export var rpm_needle: Control
 @export var rpm_limit_anim: AnimationPlayer
@@ -11,72 +13,51 @@ extends CanvasLayer
 @export var odo_meter: RichTextLabel
 @export var gear: RichTextLabel
 @export var speed: RichTextLabel
+@export_group("debug")
+@export var curve_needle: Sprite2D
+@export var power_line: Line2D
+@export var torque_line: Line2D
 
 var chassis: RigidBody2D
 var camera: Camera2D
 var rpm: float
 var rpm_limit: float
-var true_redline: float
-var redline: float
-var hudzoom: Vector2
-var dyno_end = false
-var gears := ["R", "N", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+var true_red_line: float
+var red_line: float
+var gears := ['R', 'N', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 
 
 func _ready() -> void:
-	chassis = get_tree().get_first_node_in_group("chassis")
-	camera =  get_tree().get_first_node_in_group("camera")
-	true_redline = chassis.engine.red_line_rpm + chassis.engine.rpm_limit
-	redline = chassis.engine.red_line_rpm
-	rpm_limit_progress.value = (8000.0 - true_redline)
-	rpm_redline_progress.value = (8000.0 - redline)
+	initalize()
 
 func _process(_delta: float) -> void:
-	hudzoom = camera.zoom
-	
+	hudShake(tacho, camera.zoom)
+
+	rpmProgress()
+	calculate.rpmLimiter(chassis.wheel_rpm, red_line, true_red_line, rpm_limit_anim, "rpm_limiter", rpm_progress, Color(1.0, 0.275, 0.275))
+	calculate.odoHud(odo_meter, chassis.distance_overall)
+	calculate.gearHud(gear, gears, chassis.input_gear_i)
+	calculate.speedHud(speed, chassis.kph)
+
+	curve_needle.position.x = power_line.position.x + (chassis.wheel_rpm * 0.05)
+
+func initalize():
+	chassis = get_tree().get_first_node_in_group("chassis")
+	camera =  get_tree().get_first_node_in_group("camera")
+	true_red_line = chassis.engine.red_line_rpm + chassis.engine.rpm_limit
+	red_line = chassis.engine.red_line_rpm
+	rpm_limit_progress.value = (8000.0 - true_red_line)
+	rpm_red_line_progress.value = (8000.0 - red_line)
+
+	calculate.dyno(chassis.engine.aux_line_rpm, power_line, torque_line, chassis.curve.power_curve, chassis.curve.torque_curve)
+
+
+func rpmProgress():
 	var tween := create_tween()
 	var tween2 := create_tween()
 	tween.tween_property(rpm_needle,"rotation_degrees",clampf((chassis.wheel_rpm / 8000.0) * 239, 0.0, 239.0), 0.066)
 	tween2.tween_property(rpm_progress,"value",chassis.wheel_rpm, 0.066)
 
-	odo_meter_hud(chassis.distance_overall * 0.0001)
-	gear_hud(chassis.input_gear_i)
-	speed_hud(chassis.kph)
-
-
-	rpm_limiter()
-	hudshake()
-	torque_curve()
-
-func rpm_limiter():
-	if chassis.wheel_rpm > true_redline * 1.02:
-		rpm_limit_anim.stop()
-		rpm_progress.modulate = Color(1.0, 0.27, 0.27, 1.0)
-	elif chassis.wheel_rpm > redline:
-		rpm_limit_anim.play("rpm_limiter")
-	else:
-		rpm_limit_anim.stop()
-
-func hudshake():
+func hudShake(control:Control, zoom:Vector2):
 	var tween := create_tween()
-	tween.tween_property(tacho, "scale", hudzoom, 0.041)
-
-func torque_curve():
-	if dyno_end != true:
-		if chassis.wheel_rpm > true_redline:
-			dyno_end = true
-		else:
-			$Power.add_point(Vector2(chassis.wheel_rpm * 0.05, -chassis.curve.power_curve.sample(chassis.wheel_rpm) * 1.34102209), 0)
-			$Torque.add_point(Vector2(chassis.wheel_rpm * 0.05, -chassis.curve.torque_curve.sample(chassis.wheel_rpm)), 0)
-		
-func odo_meter_hud(distance_travelled):
-	odo_meter.text = "%07d" % distance_travelled
-	var last_char = "[color=orange]%s[/color]" % odo_meter.text[-1]
-	odo_meter.text[-1] = ""
-	odo_meter.text += last_char
-
-func gear_hud(gear_i):
-	gear.text = str(gears[gear_i])
-
-func speed_hud(kph):
-	speed.text = str("%d\n[i]km/h[/i]" % kph)
+	tween.tween_property(control, "scale", zoom, 0.041)
